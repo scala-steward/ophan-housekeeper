f8bfa04da1816e06f9f439cb25e3c9f8af19cefc
Roberto Tyley <roberto.tyley@theguardian.com>
2019-10-02 16:22:11 +0100

    Add a logging alternative to the unreliable AWSRequestId

--- src/main/scala/housekeeper/LogstashLayoutWithBuildInfo.scala
+++ src/main/scala/housekeeper/LogstashLayoutWithBuildInfo.scala
@@ -3,19 +3,22 @@ package housekeeper
 import ch.qos.logback.classic.spi.ILoggingEvent
 import com.amazonaws.services.lambda.runtime.LambdaRuntimeInternal
 import net.logstash.logback.layout.LogstashLayout
-import play.api.libs.json.Json
+import play.api.libs.json.Json.{stringify, toJsObject}
+
+object UniqueIdForVM {
+  val id = java.util.UUID.randomUUID.toString
+}
 
 class LogstashLayoutWithBuildInfo extends LogstashLayout {
   LambdaRuntimeInternal.setUseLog4jAppender(true)
 
-  def getContextTags: Map[String, String] = Map(
+  val contextTags: Map[String, String] = Map(
     "buildNumber" -> BuildInfo.buildNumber,
-    "gitCommitId" -> BuildInfo.gitCommitId
+    "gitCommitId" -> BuildInfo.gitCommitId,
+    "uniqueIdForVM" -> UniqueIdForVM.id // 'AWSRequestId' seems only intermittently available, this is an alternative
   )
 
-  def makeCustomFields(customFields: Map[String, String]): String = Json.stringify(Json.toJsObject(customFields))
-
-  setCustomFields(makeCustomFields(getContextTags))
+  setCustomFields(stringify(toJsObject(contextTags)))
 
   override def doLayout(event: ILoggingEvent): String = super.doLayout(event)+"\n"
 }

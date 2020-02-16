scala-steward/ophan-housekeeper

commit 31cec53c65727e5e22e46a377bd156916ae88245

Add SNS alerting when the Ophan AWS account is sending a dangerous number of bouncing emails
The AWS SNS topic provided by AWS SES to notify us about bounces is FAR TOO NOISY,
it tells us about all bounces, even transient ones (when a user goes on holiday and sets
auto-respond), so it's very hard to see when problems arise.

This change creates a new SNS topic which we will only publish to when emails are bouncing
'permanently' and have in fact been 'suppressed' by AWS SES for failing too many times.
We can now safely subscribe the Ophan Dev and Data Tech teams to this SNS topic without
fear of bombardment.

https://groups.google.com/a/guardian.co.uk/d/msg/ophan.dev/Ckk1wcDjksA/0ZRwGfcSAgAJ

src/main/scala/housekeeper/AWS.scala
@@ -1,0 +1,18 @@
+package housekeeper
+
+import com.amazonaws.auth.{AWSCredentialsProviderChain, EC2ContainerCredentialsProviderWrapper}
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions.EU_WEST_1
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient
+import com.amazonaws.services.sns.AmazonSNSAsyncClient
+
+object AWS {
+  val credentials = new AWSCredentialsProviderChain(
+    new EC2ContainerCredentialsProviderWrapper,
+    new ProfileCredentialsProvider("ophan")
+  )
+
+  val SNS = AmazonSNSAsyncClient.asyncBuilder().withCredentials(credentials).withRegion(EU_WEST_1).build()
+
+  val Dynamo = AmazonDynamoDBAsyncClient.asyncBuilder().withCredentials(credentials).withRegion(EU_WEST_1).build()
+}

build.sbt
@@ -22,7 +22,8 @@ libraryDependencies ++= Seq(
   "org.slf4j" % "log4j-over-slf4j" % "1.7.28", //  log4j-over-slf4j provides `org.apache.log4j.MDC`, which is dynamically loaded by the Lambda runtime
   "ch.qos.logback" % "logback-classic" % "1.2.3",
 
-  "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.632",
+  "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.11.642",
+  "com.amazonaws" % "aws-java-sdk-sns" % "1.11.642",
   "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.9.3", // So many Snyk warnings
   "com.typesafe.play" %% "play-json" % "2.7.4",
   "org.scanamo" %% "scanamo" % "1.0.0-M11",

cfn.yaml
@@ -8,6 +8,7 @@ Parameters:
 Resources:
   ExecutionRole:
     Type: AWS::IAM::Role
+    DependsOn: PermanentEmailBounceTopic
     Properties:
       AssumeRolePolicyDocument:
         Statement:
@@ -40,22 +41,37 @@ Resources:
               Resource:
                 - "arn:aws:dynamodb:eu-west-1:021353022223:table/ophan-alerts"
                 - "arn:aws:dynamodb:eu-west-1:021353022223:table/DEV-ophan-alerts"
+            Statement:
+              Effect: Allow
+              Action:
+                - sns:Publish
+              Resource: !Ref PermanentEmailBounceTopic
   BounceSNSTopic:
     Type: "AWS::SNS::Topic"
     DependsOn: Lambda
     Properties:
-      DisplayName: Housekeeping SNS topic to alert when AWS SES emails bounce from Ophan Dashboard alerts
+      DisplayName: SNS topic alerted when AWS SES emails bounce (eg from Ophan Dashboard alerts, or Airflow)
       Subscription:
         - Protocol: lambda
           Endpoint: !GetAtt Lambda.Arn
       TopicName: ses-email-bounce-notifications-for-housekeeper
+
+  PermanentEmailBounceTopic:
+    Type: AWS::SNS::Topic
+    Properties:
+      DisplayName: Threat to email-sending ability of Ophan's AWS account
+
   Lambda:
     Type: AWS::Lambda::Function
+    DependsOn: PermanentEmailBounceTopic
     Properties:
       Code:
         S3Bucket: ophan-dist
         S3Key: ophan/PROD/housekeeper/housekeeper.jar
       Description: Housekeeping for Ophan
+      Environment:
+        Variables:
+          PermanentEmailBounceTopicArn: !Ref PermanentEmailBounceTopic
       Handler: housekeeper.Lambda::handler
       MemorySize: 512
       Role: !GetAtt ExecutionRole.Arn

src/main/scala/housekeeper/AlertDeletion.scala
@@ -1,8 +1,6 @@
 package housekeeper
 
 import cats.implicits._
-import com.amazonaws.regions.Regions.EU_WEST_1
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient
 import housekeeper.Dynamo.OphanAlert
 import org.scanamo._
 import org.scanamo.syntax._
@@ -10,7 +8,7 @@ import org.scanamo.auto._
 
 object Dynamo extends Logging {
 
-  val scanamo = Scanamo(AmazonDynamoDBAsyncClient.asyncBuilder().withRegion(EU_WEST_1).build())
+  val scanamo = Scanamo(AWS.Dynamo)
 
   case class OphanAlert(
                          email: String,

src/main/scala/housekeeper/Lambda.scala
@@ -2,6 +2,7 @@ package housekeeper
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.SNSEvent
+import com.amazonaws.services.sns.model.PublishRequest
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
 import scala.collection.JavaConverters._
@@ -15,16 +16,23 @@ object Lambda extends Logging {
 
     val bouncedAddresses = bounce.bouncedEmailAddresses.toSeq.sorted
 
+    val senderOfBouncedEmail = bounceNotification.mail.source
+    val bounceSummary = s"$senderOfBouncedEmail sent a ${bounce.bounceType} bounced message to ${bouncedAddresses.mkString(",")}"
     logger.info(Map(
       "bounce.type" -> bounce.bounceType,
       "bounce.subtype" -> bounce.bounceSubType,
       "bounce.permanent" -> bounce.isPermanent,
-      "bounce.mail.source" -> bounceNotification.mail.source,
+      "bounce.mail.source" -> senderOfBouncedEmail,
       "bounce.bouncedEmailAddresses" -> bouncedAddresses.asJava
-    ), s"${bounceNotification.mail.source} sent a ${bounce.bounceType} bounced message to ${bouncedAddresses.mkString(",")}")
+    ), bounceSummary)
 
     if (bounce.isPermanent) {
       bouncedAddresses.foreach(alertDeletion.deleteAllAlertsForEmailAddress)
+      if (bounce.isOnSuppressionList) {
+        val arn = sys.env("PermanentEmailBounceTopicArn")
+        logger.info(s"Sending an SNS alert to $arn")
+        AWS.SNS.publishAsync(new PublishRequest(arn, bounceSummary))
+      }
     }
   }
 

commit 887066afc61e5c38d18735fdd95e58d06c0f72f6

Fix bug loading AWS credentials that stopped alert-deletion & SNS
AWS placed our SES account under review on Saturday 15th February:

https://groups.google.com/a/guardian.co.uk/d/msg/aws-ophan/F880jhVL2yY/Wz_iVkkZAQAJ

> Your current bounce rate is 10.63%. We measured this rate over the last
> 10,028 eligible emails* you sent. Our analysis covers the last 4.3 days.

This was due to AWS-credential-loading in the Ophan Housekeeper lambda
being broken by commit 31cec53c65 back in October 2019:

https://github.com/guardian/ophan-housekeeper/pull/5/files#r379906348

I should have used `EnvironmentVariableCredentialsProvider`, not
`EC2ContainerCredentialsProviderWrapper` (see eg.
https://stackoverflow.com/a/32410565/438886 or
https://github.com/guardian/mobile-n10n/pull/156) - with credential-loading
broken, the lambda couldn't load the AWS credentials it needed to delete
entries from the `ophan-alerts` DynamoDB table, or post to the SNS topic.

Perhaps surprisingly, the Ophan Housekeeper lambda _only_ needs those AWS
credentials when it's dealing with a permanently bouncing email - so there
was no obvious problem until a permanent bounce occurred, starting at
13:26 on February 12th 2020, when these errors started:

https://logs.gutools.co.uk/s/ophan/app/kibana#/doc/581f7b00-c4df-11e9-bc08-9d1af4d9c1d5/logstash-ophan-2020.02.12?id=ciyUOXABBviLcZqRrCAI&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'2020-02-12T13:26:31.042Z',to:'2020-02-12T13:26:40.791Z'))
```
Unable to load AWS credentials from any provider in the chain: [com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper@1e730495: Unable to load credentials from service endpoint, com.amazonaws.auth.profile.ProfileCredentialsProvider@7d3a22a9: profile file cannot be null]: com.amazonaws.SdkClientException

src/main/scala/housekeeper/AWS.scala
@@ -1,14 +1,14 @@
 package housekeeper
 
-import com.amazonaws.auth.{AWSCredentialsProviderChain, EC2ContainerCredentialsProviderWrapper}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.auth.{AWSCredentialsProviderChain, EnvironmentVariableCredentialsProvider}
 import com.amazonaws.regions.Regions.EU_WEST_1
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient
 import com.amazonaws.services.sns.AmazonSNSAsyncClient
 
 object AWS {
   val credentials = new AWSCredentialsProviderChain(
-    new EC2ContainerCredentialsProviderWrapper,
+    new EnvironmentVariableCredentialsProvider,
     new ProfileCredentialsProvider("ophan")
   )
 